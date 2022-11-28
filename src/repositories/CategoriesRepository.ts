fa7e9643975cce41288f848239b9b1d69a13ff3d
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-11-28 15:54:32 -0300

    feat: create new category

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,26 @@
+import { Category } from '../model/Category';
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+export class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const newCategory = new Category();
+
+    Object.assign(newCategory, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(newCategory);
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categoires.routes';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.send('Hello world!');
-});
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () => console.log('🚀 Server is running!'));
