d9f89be6d20f608e84dda25cf85dd7eaa2081176
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-11-28 16:27:32 -0300

    feat: apply the Dependency Inversion Principle

--- src/routes/categoires.routes.ts
+++ src/routes/categoires.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+
 import { CategoriesRepository } from './../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -13,13 +15,11 @@ categoriesRoutes.get('/', (request, response) => {
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = categoriesRepository.findByName(name);
+  const createCategoriesServices = new CreateCategoryService(
+    categoriesRepository
+  );
 
-  if (category) {
-    return response.status(400).json({ error: 'Category already exists' });
-  }
-
-  categoriesRepository.create({ name, description });
+  createCategoriesServices.execute({ name, description });
 
   return response.sendStatus(201);
 });
