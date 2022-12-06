81f63f1fef31432e897825e291c6ff1f882f46b6
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-12-06 18:36:52 -0300

    feat: user admin seed

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,20 +2,17 @@ import express, { NextFunction, Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import 'express-async-errors';
 
-import '@shared/infra/typeorm';
+import createConnection from '@shared/infra/typeorm';
 import '@shared/container';
 import { router } from './routes';
 import swaggerFile from '../../../swagger.json';
 import { AppError } from '@shared/errors/AppError';
 
+createConnection();
 const app = express();
-
 app.use(express.json());
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
 app.use(router);
-
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
