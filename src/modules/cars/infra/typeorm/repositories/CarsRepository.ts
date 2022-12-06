dedab5f6b8a13b83bec4a8e7a6298f94f49071e2
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-12-06 18:02:20 -0300

    feat: car repository

---
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -0,0 +1,46 @@
+import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { Repository, getRepository } from 'typeorm';
+import { Car } from '../entities/Car';
+
+class CarsRepository implements ICarsRepository {
+  private repository: Repository<Car>;
+
+  constructor() {
+    this.repository = getRepository(Car);
+  }
+
+  async findByName(name: string): Promise<Car> {
+    return await this.repository.findOne({ name });
+  }
+
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    return await this.repository.findOne({ license_plate });
+  }
+
+  async create({
+    name,
+    description,
+    daily_rate,
+    license_plate,
+    fine_amount,
+    brand,
+    category_id,
+  }: ICreateCarDTO): Promise<Car> {
+    const car = this.repository.create({
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+
+    await this.repository.save(car);
+
+    return car;
+  }
+}
+
+export { CarsRepository };
