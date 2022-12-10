7568731022bffbd208f0b4522a4d2156211b6d69
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-12-10 14:31:44 -0300

    feat: create rentals migration

--- src/shared/infra/typeorm/migrations/1669997691363-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1669997691363-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1669997691363 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
