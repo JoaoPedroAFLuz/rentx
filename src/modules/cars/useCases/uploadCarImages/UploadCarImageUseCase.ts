43e2aa3e0cae2cd5fc263864365434e15c637fc2
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-01-24 20:20:04 -0300

    feat: save cars image in amazon s3

--- src/modules/cars/useCases/uploadCarImages/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImageUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import { ICarsImagesRepository } from '@modules/cars/repositories/ICarsImagesRepository';
+import { IStorageProvider } from '@shared/container/providers/storageProvider/IStorageProvider';
 
 interface IRequest {
   car_id: string;
@@ -11,13 +12,16 @@ interface IRequest {
 class UploadCarImageUseCase {
   constructor(
     @inject('CarsImagesRepository')
-    private carsImagesRepository: ICarsImagesRepository
+    private carsImagesRepository: ICarsImagesRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
-    images_name.forEach((image_name) =>
-      this.carsImagesRepository.create({ car_id, image_name })
-    );
+    images_name.forEach(async (image_name) => {
+      await this.carsImagesRepository.create({ car_id, image_name });
+      await this.storageProvider.save(image_name, 'cars');
+    });
   }
 }
 
